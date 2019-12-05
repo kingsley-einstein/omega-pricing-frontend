295d38af020c5ff190d7f144f1f51038f8131e7a
Kingsley Victor <javaprodigy56@gmail.com>
2019-12-05 13:35:04 +0100

    build app

---
+++ build/precache-manifest.ac87609943eba3b80ef3c9a9c990d5dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cad60e038d393b68fede2fdaead459f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b514fab0d80e164315b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cf7549c223dffbf289bf",
+    "url": "/static/js/2.c56cbe57.chunk.js"
+  },
+  {
+    "revision": "9b514fab0d80e164315b",
+    "url": "/static/js/main.f17bdc09.chunk.js"
+  },
+  {
+    "revision": "69a9a96f285521b193f3",
+    "url": "/static/js/runtime-main.49212190.js"
+  }
+]);
